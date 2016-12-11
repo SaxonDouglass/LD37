0de3884e18fda73c5e1f8ae3fc786f2cdf7725c5
Saxon Douglass <saxon@saxondouglass.com>
2016-12-11 15:36:05 +1030

    Improves canvas scaling
    Uses a logical resolution of 1920x1080 pixels which is
    scaled to fit the window/device with a centered letterbox
    if required. Pink background for testing.

--- js/load.js
+++ js/load.js
@@ -1,7 +1,9 @@
 
 var loadState = {
   preload: function () {
-    
+    game.scale.pageAlignHorizontally = true;
+    game.scale.parentIsWindow = true;
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
   },
   
   create: function () {
--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, "game");
+var game = new Phaser.Game(1920, 1080, Phaser.AUTO, "");
 
 // TODO: Implement additional game states
-// game.state.add("load", loadState);
-// game.state.add("menu", menuState);
+game.state.add("load", loadState);
+game.state.add("menu", menuState);
 // game.state.add("blueprint", blueprintState);
 // game.state.add("behaviour", behaviourState);
 game.state.add("battle", battleState);
@@ -284,4 +284,4 @@ var world = {
   tick: 0
 };
 
-game.state.start("battle");
+game.state.start("load");
